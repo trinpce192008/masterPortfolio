e30636e471821492ece0d314c482e9b46fe08970
Nguyễn Phạm Trí <trinp.ce192008@gmail.com>
2026-03-29 22:42:29 +0700

    Update greeting information in portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -19,11 +19,11 @@ const seo = {
 
 //Home Page
 const greeting = {
-  title: "Ashutosh Hathidara",
-  logo_name: "AshutoshHathidara",
-  nickname: "layman_brother",
+  title: "Nguyen Pham Tri",
+  logo_name: "Nguyen Pham Tri",
+  nickname: "TriNPCE192008",
   subTitle:
-    "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
+    "Cybersecurity Student",
   resumeLink:
     "https://drive.google.com/file/d/1bXRknv_h-XI_3CQ3SGPteGODtvEb7YvI/view?usp=sharing",
   portfolio_repository: "https://github.com/ashutosh1919/masterPortfolio",
